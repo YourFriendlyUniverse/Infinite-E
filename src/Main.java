dccb811d428dbfce2cc1588cf7e6207199252136
YourFriendlyUniverse <leontomlol1919@gmail.com>
2024-11-12 10:58:28 -0500

    added damage calculation, damage parser and more player stats

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -10,24 +11,38 @@ public class Main {
         //tests
 
         // player welcome and name selection
-        System.out.print("Welcome player to Infinite E, please type your name: ");
-        String name = s.nextLine();
+//        System.out.print("Welcome player to Infinite E, please type your name: ");
+//        String name = s.nextLine();
 
         // ##variable and class initialization##
+        Player user = new Player("e");
+        boolean run = true; // loop variable
         // all moves and damage
         String[] moves = getFileData("src/moves").toArray(new String[0]);
 
         String[] moveNames = new String[moves.length];
         String[] moveResults = new String[moves.length];
 
-        for (int i = 0; i < moves.length; i++){
+        for (int i = 0; i < moves.length; i++) {
             // parses the String for the move's name and length
             String moveHappenings = moves[i].substring(moves[i].indexOf(" ") + 1);
-            String moveName = moves[i].substring(0,moves[i].indexOf(":"));
+            String moveName = moves[i].substring(0, moves[i].indexOf(":"));
             System.out.println(moveName + ": " + moveHappenings);
             moveNames[i] = moveName;
             moveResults[i] = moveHappenings;
         }
+
+        // move happenings
+        int move = Integer.parseInt(s.nextLine());
+        System.out.println(moveNames[move]);
+        System.out.println(moveDamage(moveResults[move]));
+
+
+//        int damage = (int) ((((2 * user.getLevel()) / 5 + 2 ) * enemy_damage * (user.getAtk() / enemy_defence)) / 50) + 2 * modifier;
+        // modifer is a random number from 0.85 and 1.00 (inclusive)
+        int damage = (int) ((((2 * user.getLevel()) / 5 + 2 ) * moveDamage(moveResults[move]) * (5 / 1)) / 20) + 2;
+        System.out.println(damage);
+
 
         s.close();
     }
@@ -46,5 +61,15 @@ public class Main {
         } catch (FileNotFoundException e) {
             return fileData;
         }
+    }
+
+    public static int moveDamage(String moveOutcomes){
+        String[] moveResults = moveOutcomes.split(",");
+        for (int i = 0; i < moveResults.length; i++) {
+            if (moveResults[i].contains("enemy_damage")){
+                return Integer.parseInt(moveResults[i].substring(moveResults[i].indexOf("enemy_damage: ") + 14, moveResults[i].indexOf("]")));
+            }
+        }
+        return -1;
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,20 +1,63 @@
 import java.util.ArrayList;
 
 public class Player {
-    int maxHp;
-    int hp;
-    int speed;
-    int atk;
-    ArrayList<String> moves;
+    private int maxHp;
+    private int hp;
+    private int speed;
+    private int atk;
+    private int defence;
+    private int exp;
+    private int level;
+    private String name;
+    private ArrayList<String> moves;
 
-    public Player(){
+    public Player(String name){
+        this.name = name;
         maxHp = 20;
         hp = maxHp;
         speed = 100;
         atk = 1;
+        defence = 1;
+        exp = 0;
+        level = 5;
         moves = new ArrayList<String>();
         moves.add("slap");
     }
+
+    public void damageTaken(int damage){
+        hp -= damage;
+    }
+
+    public void updateStat(String stat, int amount){
+        if (stat.equals("maxHp")){
+            maxHp += amount;
+        }
+        else if (stat.equals("speed")) {
+            speed += amount;
+        }
+        else if (stat.equals("atk")){
+            atk += amount;
+        }
+        else if (stat.equals("defence")){
+            defence += amount;
+        }
+        else{
+            System.out.println("ERROR YOU SPELT" + stat + "INCORRECTLY");
+        }
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    public void expIncrease(int amount){
+        exp += amount;
+    }
+
+    public int getAtk(){
+        return atk;
+    }
+
 
 
 }
